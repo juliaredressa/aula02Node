656db728be5733ca2245c88dd331f0377ba381e6
Julia Redressa <julialimaredressa@gmail.com>
2023-11-12 17:42:43 -0300

    ultimas alterações

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 import express from 'express';
+import path from 'path';0
 
 
 const porta = 3000;
-const host = '0.0.0.0';
+const host = 'localhost';
 var listaUsuarios = [];
 
 function processaCadastroUsuario(requisicao, resposta){
     //processar os parametros da url em http://http://localhost:3000/paginas/cadastraUsuario.html?nome=Julia&sobrenome=Redressa&nomeUsuario=juliaredressa&cidade=Presidente+Prudente&uf=SP&cep=190-50000
     const usuario = {
                 nome : requisicao.query.nome,
-                sobrenome : requisicao.query.sobrenome,
-                nomeUsuario : requisicao.query.nomeUsuario,
-                cidade : requisicao.query.cidade,
-                uf : requisicao.query.uf,
-                cep : requisicao.cep
+                preco : requisicao.query.preco,
+                codigo : requisicao.query.codigo,
+                descricao : requisicao.query.descricao,
+                categoria : requisicao.query.categoria,
+                cep : requisicao.query.cep
                 }
     //adicionar um novo usuario na lista de usuarios já cadastrados
     listaUsuarios.push(usuario);
     //retornar lista de usuarios
+    
     let conteudoDaResposta = `
                 <!DOCTYPE html>
                 <html lang="pt-br">
@@ -31,10 +33,10 @@ function processaCadastroUsuario(requisicao, resposta){
                         <thead>
                             <tr>
                                 <th>Nome</th>
-                                <th>Sobrenome</th>
-                                <th>Nome de usuario</th>
-                                <th>Cidade/UF</th>
-                                <th>CEP</th>
+                                <th>Preço</th>
+                                <th>Codigo</th>
+                                <th>Descrição</th>
+                                <th>Categoria</th>
                             </tr>
                         </thead>
                         <tbody>`;
@@ -43,23 +45,26 @@ function processaCadastroUsuario(requisicao, resposta){
             conteudoDaResposta += `
                                     <tr>
                                         <td>${usuario.nome}</td>
-                                        <td>${usuario.sobrenome}</td>
-                                        <td>${usuario.nomeUsuario}</td>
-                                        <td>${usuario.cidade}/${usuario.uf}</td>
-                                        <td>${usuario.cep}</td>
-                                    </tr>`;}
+                                        <td>${usuario.preco}</td>
+                                        <td>${usuario.codigo}</td>
+                                        <td>${usuario.descricao}</td>
+                                        <td>${usuario.categoria}</td>
+                                    </tr>
+                                `;
+        }
 
         conteudoDaResposta+=`
                                 </tbody>
                             </table>
-                            <a class="btn btn-primary href="/" role="button">Voltar ao menu</a>
-                            <a class="btn btn-primary href="/cadastraUsuario.html" role="button">Continuar cadastrando</a>
+                            <a class="btn btn-primary" href="/" role="button">Voltar ao menu</a>
+                            <a class="btn btn-primary" href="/cadastraUsuario.html" role="button">Continuar cadastrando</a>
 
                             </body>
                             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
                             </script>
-                            <html>`;
-        resposta.end(conteudoDaResposta);
+                            <html>
+                            `
+    resposta.end(conteudoDaResposta);
 }
 
 const app = express();
@@ -72,21 +77,18 @@ app.get('/', (requisicao, resposta) => {
             <!DOCTYPE html>
                 <html lang="pt-br">
             <head>
-                <title>Menu do sistema</title>
+                <title>Cadastro de produtos</title>
             </head>
             <body>
                 <h1>MENU</h1>
                 <ul>
-                    <li><a href="/cadastraUsuario.html">Cadastrar usuario</a><li>
+                    <li><a href="/cadastraUsuario.html">Cadastrar produtos</a></li>
                 </ul>
             </body>
             </html>
     `)
-});
-
-//rota para processar o cadastro de usuarios endpoin = '/cadastraUsuarios.html'
+})
 app.get('/cadastraUsuario', processaCadastroUsuario)
-
 app.listen(porta, host, () => {
     console.log(`Servidor executando na url http://${host}:${porta}`);
 });
